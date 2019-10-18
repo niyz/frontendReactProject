41c0d69bd91664849c9d5a42eac210cb6d819b8c
Pär <parso89@gmail.com>
2019-10-18 16:18:56 +0200

    added resturants

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReviewList from './containers/ReviewList/ReviewList';
+import TopRestaurants from "./containers/TopRestaurants/TopRestaurants";
 
 //Rename header -> ReviewContainer
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App container"> 
-        <ReviewList></ReviewList>
-        
+      <div className="App container">
+          <ReviewList></ReviewList>
+          <TopRestaurants></TopRestaurants>
       </div>
+
     ); 
   }
 }
---
+++ src/containers/TopRestaurants/TopRestaurants.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import RestaurantCard from "../../components/RestaurantCard/RestaurantCard";
+
+const axios = require('axios');
+var counter =0;
+
+class TopRestaurants extends React.Component {
+    state = {
+        restaurants: []
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:8080/restaurant/latest')
+            .then((result) => {
+                console.log(result.data);
+                return result.data;
+            })
+            .then((data) => {
+                this.setState({restaurants: data})
+                console.log("state was set");
+            })
+            .catch((error) => {
+                console.log("ERROR " + error)
+            })
+    }
+    render() {
+        counter++;
+        console.log(counter);
+        const cards = this.state.restaurants.map(value2 => (
+            <RestaurantCard
+                restaurantId={value2.restaurant_id}
+                name={value2.name}
+                address={value2.address}
+                category={value2.category}
+                description={value2.description}
+
+            />
+
+        ));
+
+        return(
+            <React.Fragment>
+                <br/>
+                <h4>Resturants</h4>
+                <hr/>
+                <div className="row">
+                    { cards }
+                </div>
+            </React.Fragment>
+
+        )
+    }
+
+}
+
+export default TopRestaurants;
