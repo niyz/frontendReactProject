d678d39da2fbb9e1b7c828988f28678d8511aa01
Simon Nilsson <simon.nilsson123@gmail.com>
2019-10-19 15:10:11 +0200

    Register function and button
    Able to push register button to visit /register and able to register a user.

--- src/containers/Header/Navbar.js
+++ src/containers/Header/Navbar.js
@@ -1,5 +1,9 @@
 import './Navbar.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
+
 
 
 const Navbar = (props) => {
@@ -11,7 +15,9 @@ const Navbar = (props) => {
   <form className="form-inline">
     <input className="form-control mr-sm-2" type="search" placeholder="Username" aria-label="Search"/>
     <input className="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Search"/>
-    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Register</button>
+    <Button classname ="btn btn-outline-success my-2 my-sm-0" component={Link} to="/register">    
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Register</button>  
+    </Button>
     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
   </form>      
   
