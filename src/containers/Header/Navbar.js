87e13a64695ef449ff67753cc417c41093c3755c
Simon Nilsson <simon.nilsson123@gmail.com>
2019-10-19 11:17:52 +0200

    Class -> Classname

--- src/containers/Header/Navbar.js
+++ src/containers/Header/Navbar.js
@@ -5,19 +5,19 @@ import React from 'react';
 const Navbar = (props) => {
 
     return (
-        <div class="container">
-<nav class="navbar navbar-dark bg-dark">
+        <div className="container">
+<nav className="navbar navbar-dark bg-dark">
   
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Username" aria-label="Search"/>
-    <input class="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Search"/>
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Register</button>
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+  <form className="form-inline">
+    <input className="form-control mr-sm-2" type="search" placeholder="Username" aria-label="Search"/>
+    <input className="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Search"/>
+    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Register</button>
+    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
   </form>      
   
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  <form className="form-inline">
+    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 
   </form>
 </nav> 
